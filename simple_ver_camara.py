c94cd0272684d3e08e7d30f7d6f169c762645d63
Ric LS <ricardoutn@gmail.com>
2022-06-03 15:05:09 -0300

    simple

---
+++ simple_ver_camara.py
@@ -0,0 +1,22 @@
+import cv2
+cam = cv2.VideoCapture(0)
+frame_width = int( cam.get(cv2.CAP_PROP_FRAME_WIDTH))
+frame_height =int( cam.get( cv2.CAP_PROP_FRAME_HEIGHT))
+cv2.namedWindow("test")
+
+while cam.isOpened():
+
+    ret, frame = cam.read()
+    if not ret:
+        print("failed to grab frame")
+        break
+    cv2.imshow("test", frame)
+
+    k = cv2.waitKey(1)
+    if k%256 == 27:
+        # ESC pressed
+        print("Escape hit, closing...")
+        break
+
+cam.release()
+cv2.destroyAllWindows()
